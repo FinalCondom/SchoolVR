39c0eb516e20cb88c8d5213016a92183c1e2ed48
lucien <lucienzuber@gmail.com>
2017-05-19 14:30:03 +0200

    Base Menu Working
    We now have the base menu

---
+++ SchoolVRUnity/Assets/Scripts/MenuTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuTimer : MonoBehaviour {
+    public Text text;
+    private float timer = 0;
+    private SceneManager manager = new SceneManager();
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 0)
+        {
+            text.text = "SchoolVR";
+        }
+        if (timer > 4)
+        {
+            text.text = "Venez visiter le Valais !";
+        }
+        if (timer > 8)
+        {
+            text.text = "Choisissez ce que vous voulez voir !";
+        }
+        if (timer > 12)
+        {
+            manager.loadLevel("SelectionMenu");
+        }
+        //text.text = "TIMER : " + timer;
+    }
+}
