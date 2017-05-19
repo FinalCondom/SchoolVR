87b1e3ce323ac2b01878bda3019a19daccaed1c6
lucien <lucienzuber@gmail.com>
2017-05-19 20:49:31 +0200

    WithAudio
    With Audio Working

---
+++ SchoolVRUnity/Assets/Scripts/MusicAwakeCheck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAwakeCheck : MonoBehaviour {
+    public bool musicPlaying = false;
+	// Use this for initialization
+	void Start () {
+        musicPlaying = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SchoolVRUnity/Assets/Scripts/StartAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAudio : MonoBehaviour {
+    public AudioClip impact;
+    public List<AudioSource> musics;
+    AudioSource audio;
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        //transform.position += new Vector3(0f,1f,0f);
+        bool play = false;
+        foreach(AudioSource source in musics)
+        {
+            if (source.isPlaying)
+            {
+                play = true;
+            }
+        }
+        if (!play)
+        {
+            audio.clip = impact;
+            audio.Play();
+        }
+    }
+}
