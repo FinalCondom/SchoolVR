c43cc77a09a0b5f41596d2781a70180f87ad62e2
Lberthod <lberthod@gmail.com>
2017-05-19 19:32:38 +0200

    modif the selectSceneView
    modif finge image

---
+++ SchoolVRUnity/Assets/Scripts/GoScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoScene : MonoBehaviour {
+
+
+    private SceneManager manager = new SceneManager();
+    public string levelTag;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void changeScene()
+    {
+        manager.loadLevel(levelTag);
+    }
+}
---
+++ SchoolVRUnity/Assets/Scripts/MusiqueTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusiqueTrigger : MonoBehaviour {
+
+    public AudioClip impact;
+    AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+
+    }
+
+    public void PlayMusic()
+    {
+        //transform.position += new Vector3(0f,1f,0f);
+        audio.clip = impact;
+        audio.Play();
+
+
+    }
+}
